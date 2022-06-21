773600a491e6af023833d5b298d4982b25a67a09
FavourEEZ <favouromoragbon2002@gmail.com>
2022-06-21 23:14:37 +0100

    Add skelenton FRS

--- src/App.js
+++ src/App.js
@@ -5,19 +5,20 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> Favour's Movie Club </h1>
+
       </header>
+      <div className="main-wrapper">
+        <h3>Functional Requirement Specification</h3>
+        <ul>
+          <li> Portal for members to sign in </li>
+          <li> View upcoming movies on Home Page </li>
+          <li> Only members can see location </li>
+          <li> Member focused images of them enjoying the experience </li>
+          <li> See previous movies that have been watched at the club and corressponding photos  </li>
+
+        </ul>
+      </div>
     </div>
   );
 }
