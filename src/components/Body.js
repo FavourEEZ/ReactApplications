d26fa2914744e4dc1c1e54a627f110d85b51d981
FavourEEZ <favouromoragbon2002@gmail.com>
2022-07-01 17:27:53 +0100

    clean up UI

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,40 +5,34 @@ import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Body() {
-    let items = [
-        {
-            name: "Random Name #1",
-            description: "Probably the most random thing you have ever seen!"
-        },
-        {
-            name: "Random Name #2",
-            description: "Hello World!"
-        }
-    ]
+
     return (
     <div className="">
-        <h3>Functional Requirement Specification</h3>
-        <ul>
-            <li> Portal for members to sign in </li>
-            <li> View upcoming movies on Home Page </li>
-            <li> Only members can see location </li>
-            <li> Member focused images of them enjoying the experience </li>
-            <li> See previous movies that have been watched at the club and corressponding photos  </li>
+        <section className="FRS-vision">
+            <h3>Functional Requirement Specification</h3>
+            <ul>
+                <li> Portal for members to sign in </li>
+                <li> View upcoming movies on Home Page </li>
+                <li> Only members can see location </li>
+                <li> Member focused images of them enjoying the experience </li>
+                <li> See previous movies that have been watched at the club and corressponding photos  </li>
 
-        </ul>
+            </ul>
 
-        <h3> Vision for the Club </h3>
-        <ul> 
-            <li> Will be London Based </li>             
-            <li> USing NFTs for entry - Will be sent out to members </li>
-            <li> Movies will be advertised because day </li>
-            <li> Once a month? </li>
-            <li> Free </li>
-            <li> Providing the best movie experience, good vibes, food </li>
-            <li> Deep connections/communitry - This is the power </li>
-            <li> Exclusivity: generate international interest - Have another NFT collection for people to win a spot</li>
-        </ul>
-        <Button>Click!</Button>
+            <h3> Vision for the Club </h3>
+            <ul> 
+                <li> Will be London Based </li>             
+                <li> USing NFTs for entry - Will be sent out to members </li>
+                <li> Movies will be advertised because day </li>
+                <li> Once a month? </li>
+                <li> Free </li>
+                <li> Providing the best movie experience, good vibes, food </li>
+                <li> Deep connections/communitry - This is the power </li>
+                <li> Exclusivity: generate international interest - Have another NFT collection for people to win a spot</li>
+            </ul>
+            <Button>Begin</Button>
+        </section>
+
         <Carousel>
             <Carousel.Item>
                 <img
@@ -76,6 +70,7 @@ export default function Body() {
                 </Carousel.Caption>
             </Carousel.Item>
         </Carousel>
+
         <footer id='footer'>
             <small>© 2022 BlueOrange development. All rights reserved.</small>
         </footer>
