af82117e2ac362eceaeeef21e7aaa7546f51698f
FavourEEZ <favouromoragbon2002@gmail.com>
2022-07-02 18:24:36 +0100

    Add Carousel to landing page view

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
 import './App.css';
-import Container from '@mui/material/Container'
 import Button from '@mui/material/Button';
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+
 
 export default function App() {
 
@@ -36,11 +38,46 @@ export default function App() {
         </div>
         
         <div className='wrapper-landing-content'>
-          <Container maxWidth="sm" className='Container'>
-            <img width={"250px"} height={"250px"} src='https://images.unsplash.com/photo-1458053688450-eef5d21d43b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80'></img>
-            <h3>Join the experience</h3>
+          <Carousel>
+            <Carousel.Item>
+              <img
+              className="d-block w-100"
+              src="https://images.unsplash.com/photo-1458053688450-eef5d21d43b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"///800x400?text=Second slide&bg=282c34"
+              alt="First slide"
+              />
+              <Carousel.Caption>
+              <h3>Join the experience</h3>
+              {/* <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p> */}
+              </Carousel.Caption>
+            </Carousel.Item>
 
-          </Container>
+            <Carousel.Item>
+              <img
+              className="d-block w-100"
+              src="group-cinema-img.jpeg"///800x400?text=Third slide&bg=20232a"
+              alt="Second slide"
+              />
+
+              <Carousel.Caption>
+              <h3>Meet new people</h3>
+              {/* <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p> */}
+              </Carousel.Caption>
+            </Carousel.Item>
+
+            <Carousel.Item>
+              <img
+              className="d-block w-100"
+              src="networkingImage.jpg"///800x400?text=First slide&bg=373940"
+              alt="Third slide"
+              />
+
+              <Carousel.Caption>
+              <h3>Have your perfect movie night</h3>
+              {/* <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p> */}
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel>
+
           <div className='join-button'>
             <Button variant="contained" className=''> Join waiting list </Button>
             <Button variant="contained" color='success' className=''> Movies </Button>
--- src/components/Body.js
+++ src/components/Body.js
@@ -1,7 +1,6 @@
 import React from "react";
 import '../Body.css';
 import Button from 'react-bootstrap/Button';
-import Carousel from 'react-bootstrap/Carousel';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default function Body() {
@@ -32,44 +31,6 @@ export default function Body() {
             </ul>
             <Button>Begin</Button>
         </section>
-
-        <Carousel>
-            <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src="https://images.unsplash.com/photo-1458053688450-eef5d21d43b3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1173&q=80"///800x400?text=Second slide&bg=282c34"
-                alt="First slide"
-                />
-                <Carousel.Caption>
-                <h3>First slide label</h3>
-                <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src="group-cinema-img.jpeg"///800x400?text=Third slide&bg=20232a"
-                alt="Second slide"
-                />
-
-                <Carousel.Caption>
-                <h3>Second slide label</h3>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item>
-                <img
-                className="d-block w-100"
-                src="networkingImage.jpg"///800x400?text=First slide&bg=373940"
-                alt="Third slide"
-                />
-
-                <Carousel.Caption>
-                <h3>Third slide label</h3>
-                <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-                </Carousel.Caption>
-            </Carousel.Item>
-        </Carousel>
 
         <footer id='footer'>
             <small>© 2022 BlueOrange development. All rights reserved.</small>
